948721efb966de167b572873ce6755e2ab9f318c
Guillaume Vareille <tinyfiledialogs@ysengrin.com>
2017-06-13 03:42:28 +0200

    v2.8.4

--- tinyfiledialogs.h
+++ tinyfiledialogs.h
@@ -1,5 +1,5 @@
 /*_________
- /         \ tinyfiledialogs.h v2.8.4 [Jun 11, 2017] zlib licence
+ /         \ tinyfiledialogs.h v2.8.4 [Jun 13, 2017] zlib licence
  |tiny file| Unique header file created [November 9, 2014]
  | dialogs | Copyright (c) 2014 - 2017 Guillaume Vareille http://ysengrin.com
  \____  ___/ http://tinyfiledialogs.sourceforge.net

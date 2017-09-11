4decca63b37ef75375f8ca8dc6f65334b520ea83
Guillaume Vareille <tinyfiledialogs@ysengrin.com>
2017-09-11 09:28:31 +0200

    v3.0.1

--- tinyfiledialogs.c
+++ tinyfiledialogs.c
@@ -609,12 +609,12 @@ static void RGB2HexW(
 	{
 		if (aRGB)
 		{
-#if defined(__GNUC__) && __GNUC__ < 5
-swprintf(aoResultHexRGB, L"#%02hhx%02hhx%02hhx", aRGB[0], aRGB[1], aRGB[2]);
-#else
-swprintf(aoResultHexRGB, 8, L"#%02hhx%02hhx%02hhx", aRGB[0], aRGB[1], aRGB[2]);
+			/* wprintf(L"aoResultHexRGB %s\n", aoResultHexRGB); */
+			swprintf(aoResultHexRGB,
+#if !defined(__GNUC__) || (__GNUC__) >= 5
+				8,
 #endif
-		 /* wprintf(L"aoResultHexRGB %s\n", aoResultHexRGB); */
+			L"#%02hhx%02hhx%02hhx", aRGB[0], aRGB[1], aRGB[2]);
 		}
 		else
 		{

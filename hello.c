22ae41d949e05b220247e9a9b58236c231e0a160
Guillaume Vareille <tinyfiledialogs@ysengrin.com>
2018-04-02 10:58:50 +0200

    v3.3.4

--- hello.c
+++ hello.c
@@ -21,7 +21,7 @@ OpenFileDialog SaveFileDialog SelectFolderDialog
 Native dialog library for WINDOWS MAC OSX GTK+ QT CONSOLE & more
 SSH supported via automatic switch to console mode or X11 forwarding
 
-a C file and a header (add them to your C or C++ project) with 8 functions:
+one C file and a header (add them to your C or C++ project) with 8 functions:
 - beep
 - notify popup
 - message & question

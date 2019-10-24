b54143ebcdd533a3e91fe47a681651b6723b5fe2
Guillaume Vareille <tinyfiledialogs@ysengrin.com>
2019-10-24 21:19:50 +0200

    v3.4

--- tinyfiledialogs_dll32.bat
+++ tinyfiledialogs_dll32.bat
@@ -3,7 +3,7 @@ dlltool --export-all-symbols -l tinyfiledialogs32.lib tinyfiledialogs.o --dllnam
 gcc -shared tinyfiledialogs.o -o tinyfiledialogs32.dll -LC:/mingw/lib -lcomdlg32 -lole32
 gcc -ansi -std=c89 -pedantic -Wstrict-prototypes -o hello.exe hello.c tinyfiledialogs32.lib
 
-rem gcc -ansi -std=gnu89 -pedantic -c tinyfiledialogs.c
+rem gcc -ansi -std=gnu89 -pedantic -Wstrict-prototypes -c tinyfiledialogs.c
 rem dlltool --export-all-symbols -l tinyfiledialogs32.lib tinyfiledialogs.o --dllname tinyfiledialogs32.dll
 rem gcc -shared tinyfiledialogs.o -o tinyfiledialogs32.dll -LC:/mingw/lib -lcomdlg32 -lole32
-rem gcc -ansi -std=c89 -pedantic -o hello.exe hello.c tinyfiledialogs32.lib
+rem gcc -ansi -std=c89 -pedantic -Wstrict-prototypes -o hello.exe hello.c tinyfiledialogs32.lib

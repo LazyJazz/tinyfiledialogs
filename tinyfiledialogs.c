f042f64a9dfafa4b1e37fb7f6b3f2b77ef0a3930
Guillaume Vareille <tinyfiledialogs@ysengrin.com>
2020-04-13 14:53:16 +0200

    3.5.0

--- tinyfiledialogs.c
+++ tinyfiledialogs.c
@@ -132,7 +132,11 @@ misrepresented as being the original software.
 #endif /* _WIN32 */
 
 #define MAX_PATH_OR_CMD 1024 /* _MAX_PATH or MAX_PATH */
+
+#ifndef MAX_MULTIPLE_FILES
 #define MAX_MULTIPLE_FILES 1024
+#endif
+
 #define LOW_MULTIPLE_FILES 32
 
 char const tinyfd_version [8] = "3.5.0";
